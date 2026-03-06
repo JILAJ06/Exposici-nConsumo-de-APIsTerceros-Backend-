dd4b93619b3bfce085ed2cd0923749fd41f7daed
Alexander Jesús Jiménez León <jilaj.alexander@gmail.com>
2026-03-06 09:22:54 -0600

    Update derver.js

--- src/server.js
+++ src/server.js
@@ -8,11 +8,13 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: [
+        'http://localhost:3000', 
+        process.env.FRONTEND_URL 
+    ],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 app.use(express.json());
 
 app.use('/api/hp', hpRoutes);
